5536473541bbc2e1de4e8394a8ed97bb8ba5ede7
Shannon Brooks <552756+realslacker@users.noreply.github.com>
2022-11-28 13:13:04 -0600

    initial commit

---
+++ pwsh_profile.ps1
@@ -0,0 +1,50 @@
+#Requires -Version 7.0
+
+using namespace System.Net
+using namespace System.Security.Principal
+
+$InformationPreference = 'Continue'
+
+# remove WindowsPowerShell modules, note that you can create a symlink to a specific module if you want it available
+# ie: New-Item -ItemType SymbolicLink -Path '~\Documents\PowerShell\Modules\ActiveDirectory' -Target 'C:\Windows\System32\WindowsPowerShell\v1.0\Modules\ActiveDirectory'
+$env:PSModulePath = $env:PSModulePath.Split(';') -notlike '*\WindowsPowerShell\Modules' -join ';'
+
+# add script directories to path
+$env:Path = $env:Path, "$PSScriptRoot\Scripts", "$env:USERPROFILE\Repos\SharedScripts\Prod", "$env:USERPROFILE\Repos\SharedScripts\Test", "$env:USERPROFILE\Repos\SharedScripts\Dev" -join ';'
+
+# Bump up the max TLS profile 
+[ServicePointManager]::SecurityProtocol = [SecurityProtocolType].GetEnumValues().Where({ $_ -ge [SecurityProtocolType]::Tls12 })
+
+$env:IsDemo = [int](Test-Path ~\.demo)
+# $env:DemoUser is set in PSDefaultParameterValues
+
+function Switch-DemoMode {
+    if (Test-Path ~\.demo) {
+        Remove-Item ~\.demo -Force
+        $env:IsDemo = 0
+    } else {
+        if ( -not $env:DemoUser ) {
+            $env:DemoUser = Read-Host 'Demo User'
+        }
+        New-Item ~\.demo > $null
+        $env:IsDemo = 1
+    }
+}
+
+try {
+    oh-my-posh init pwsh --config "$PSScriptRoot\slacker.omp.json" | Invoke-Expression
+    function Set-PoshEnv {
+        $env:BW_STATUS = ('locked', 'unlocked')[$(Test-Path ~\.config\BitwardenWrapper\.unlocked)]
+    }
+    New-Alias -Name Set-PoshContext -Value Set-PoshEnv
+} catch {}
+
+Set-PSReadLineOption -Colors @{
+    Parameter        = $PSStyle.Foreground.BrightCyan
+    Operator         = $PSStyle.Foreground.BrightRed
+    InlinePrediction = $PSStyle.Foreground.BrightBlack
+} -HistorySaveStyle SaveIncrementally -HistoryNoDuplicates -PredictionSource History -BellStyle Visual
+
+$PSStyle.Formatting.Warning = $PSStyle.Blink + $PSStyle.Foreground.BrightYellow
+$PSStyle.Formatting.Verbose = $PSStyle.Foreground.Cyan
+$PSStyle.Formatting.Debug   = $PSStyle.Background.Magenta
