37e92f9098c5d5a39bca5729f1f091e8ada21da0
Shannon Graybrook <552756+realslacker@users.noreply.github.com>
2023-06-26 13:37:36 -0500

    custom console titles

--- common.ps1
+++ common.ps1
@@ -9,9 +9,9 @@ function Get-PSUAPIToken {
         $AuthorizationHeader
 
     )
-    if ( Test-Path 'C:\Users\sg503689\AppData\Roaming\code\User\settings.json' ) {
+    if ( Test-Path "$env:APPDATA\code\User\settings.json" ) {
 
-        $Tokens = (Get-Content C:\Users\sg503689\AppData\Roaming\code\User\settings.json | ConvertFrom-Json).'powerShellUniversal.connections' | Where-Object Name -like $Name
+        $Tokens = (Get-Content "$env:APPDATA\code\User\settings.json" | ConvertFrom-Json).'powerShellUniversal.connections' | Where-Object Name -like $Name
 
         if ( $AuthorizationHeader ) {
             if ( $Tokens.Count -eq 1 ) {
@@ -26,21 +26,42 @@ function Get-PSUAPIToken {
     }
 }
 
+function Set-ConsoleTitle {
+    param( [string] $Title )
+    if ( [string]::IsNullOrEmpty($Title) ) {
+        Clear-ConsoleTitle
+    } else {
+        $env:MyConsoleTitle = $Title
+    }
+}
+
+function Clear-ConsoleTitle {
+    Remove-Item Env:\MyConsoleTitle
+}
+
 # VMware Argument Completers
 Register-ArgumentCompleter -CommandName New-VM, Set-VM -ParameterName GuestId -ScriptBlock { param( $dc, $dc2, $WordToComplete ) [VMware.Vim.VirtualMachineGuestOsIdentifier].GetEnumNames().Where({ $_ -like "$WordToComplete*" }) }
 
 Register-ArgumentCompleter -ParameterName ComputerName -ScriptBlock {
     param( $CommandName, $ParameterName, $WordToComplete, $CommandAst, $FakeBoundParameters )
-    Invoke-BIDatabaseQuery -Query 'SELECT LOWER(`DNSHostName`) AS `Option` FROM `ad_Objects` WHERE `ObjectClass`="Computer" AND `Deleted`=0 AND `OperatingSystem` LIKE "%Server%" AND `DNSHostName` IS NOT NULL AND `DNSHostName` LIKE @WordToComplete' -Parameters @{
-        WordToComplete = $WordToComplete.Replace('?', '_').Replace('*', '%').TrimEnd('%') + '%'
-    } | Select-Object -Expand Option
+    if ( $CommandName -eq 'Connect-VIServer' ) {
+        'ncsa.vcenter.methode.global', 'emea.vcenter.methode.global', 'apac.vcenter.methode.global', 'pcp-vcenter1.procoplast.be' | Where-Object { $_ -match "${WordToComplete}*" }
+    } else {
+        Invoke-BIDatabaseQuery -Query 'SELECT LOWER(`DNSHostName`) AS `Option` FROM `ad_Objects` WHERE `ObjectClass`="Computer" AND `Deleted`=0 AND `OperatingSystem` LIKE "%Server%" AND `DNSHostName` IS NOT NULL AND `DNSHostName` LIKE @WordToComplete' -Parameters @{
+            WordToComplete = $WordToComplete.Replace('?', '_').Replace('*', '%').TrimEnd('%') + '%'
+        } | Select-Object -Expand Option
+    }
 }
 
 Register-ArgumentCompleter -ParameterName Server -ScriptBlock {
     param( $CommandName, $ParameterName, $WordToComplete, $CommandAst, $FakeBoundParameters )
-    Invoke-BIDatabaseQuery -Query 'SELECT LOWER(`DNSHostName`) AS `Option` FROM `ad_Objects` WHERE `ObjectClass`="Computer" AND `Deleted`=0 AND `OperatingSystem` LIKE "%Server%" AND `DNSHostName` IS NOT NULL AND `DNSHostName` LIKE @WordToComplete' -Parameters @{
-        WordToComplete = $WordToComplete.Replace('?', '_').Replace('*', '%').TrimEnd('%') + '%'
-    } | Select-Object -Expand Option
+    if ( $CommandName -eq 'Connect-VIServer' ) {
+        'ncsa.vcenter.methode.global', 'emea.vcenter.methode.global', 'apac.vcenter.methode.global', 'pcp-vcenter1.procoplast.be' | Where-Object { $_ -match "${WordToComplete}*" }
+    } else {
+        Invoke-BIDatabaseQuery -Query 'SELECT LOWER(`DNSHostName`) AS `Option` FROM `ad_Objects` WHERE `ObjectClass`="Computer" AND `Deleted`=0 AND `OperatingSystem` LIKE "%Server%" AND `DNSHostName` IS NOT NULL AND `DNSHostName` LIKE @WordToComplete' -Parameters @{
+            WordToComplete = $WordToComplete.Replace('?', '_').Replace('*', '%').TrimEnd('%') + '%'
+        } | Select-Object -Expand Option
+    }
 }
 
 New-Alias -Name npp -Value 'C:\Program Files\Notepad++\notepad++.exe'
